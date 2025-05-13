b6bae22b1e144bee9d109ce445a6a6395777bbce
AlexandreRob <alexandre.robert974@hotmail.fr>
2025-05-13 20:02:18 +0400

    Update server.js
    Ajout d'un lien pour le healthcheck
    Gestion des utilisateurs

--- backappchat/server.js
+++ backappchat/server.js
@@ -1,33 +1,46 @@
-const express = require('express');
-const http = require('http');
-const {Server} = require('socket.io');
+const express = require("express");
+const http = require("http");
+const { Server } = require("socket.io");
+const users = new Set();
 
 const app = express();
 const httpServer = http.createServer(app);
-const io = new Server (httpServer, {
-    cors: {
-      origin: "*",
-      methods: ['GET', "POST"]
-    }
+const io = new Server(httpServer, {
+  cors: {
+    origin: "*",
+    methods: ["GET", "POST"],
+  },
 });
 
-io.on('connection', (socket) => {
-    console.log(`user is connected`)
+app.get("/health", (req, res) => {
+  res.sendStatus(200);
+});
 
-    
+io.on("connection", (socket) => {
+  //   console.log(`user is connected`);
 
-    socket.on('message', data => {
-        socket.broadcast.emit('message:received', data)
-    })
+  socket.on("join", (username) => {
+    socket.username = username;
+    users.add(username);
+    io.emit("user:joined", `${username} a rejoint le chat`);
+    io.emit("users:update", Array.from(users));
+  });
 
+  socket.on("message", (data) => {
+    // socket.emit("message:sent", data);
+    socket.broadcast.emit("message:received", data);
+    // io.emit("message:received", data); // au lieu de socket.broadcast.emit
+  });
 
-
-    socket.on('disconnect', () => {
-        console.log(`user left`)
-    })
-})
+  socket.on("disconnect", () => {
+    if (socket.username) {
+      console.log(`user left`);
+      users.delete(socket.username);
+      io.emit("users:update", Array.from(users));
+    }
+  });
+});
 
 httpServer.listen(3000, () => {
-    console.log('server on: http://localhost:3000/')
-}
-)
+  console.log("server on: http://localhost:3000/");
+});
